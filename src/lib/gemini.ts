89aa3076e0a8e662f8d120e33f97802f44db2272
Douglas Lovera <doulovera@gmail.com>
2025-05-16 23:03:36 -0500

    feat: add Gemini API integration for XML generation from descriptions

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,21 +1,22 @@
 "use server";
-// imports
+
 import { GoogleGenAI } from "@google/genai";
 import * as fs from "node:fs";
 import * as path from "node:path";
-// gemini config
+
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
-// obtaining prompt from txt
+
+const MODEL = "gemini-2.5-flash-preview-04-17";
+
 const filePath = path.join(process.cwd(), "src/prompts", "description-to-xml-database.txt");
 const prompt = fs.readFileSync(filePath, "utf8");
-// generar base de datos en archivo xml basado en una descripcion
-async function generateXML(description: string): Promise<string> {
+
+export async function generateXmlFromDescription(description: string): Promise<string> {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash-preview-04-17",
-    contents: prompt + description,
+    model: MODEL,
+    contents: `${prompt} "${description}"`,
   });
+
   return response.text || "";
 }
-
-export { generateXML };
