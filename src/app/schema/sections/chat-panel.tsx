5eedee4943802b0cb93166a1ae75a477ac32fcff
Douglas Lovera <doulovera@gmail.com>
2025-05-19 02:29:57 -0500

    feat: improve styling of Chat component by adding padding to scrollable area and input section

--- src/app/schema/sections/chat-panel.tsx
+++ src/app/schema/sections/chat-panel.tsx
@@ -31,10 +31,10 @@ export default function Chat({ hidePanel }: { hidePanel: () => void }) {
       </div>
       <div className="flex-1 overflow-auto p-4 flex items-center justify-center">
         <div className="flex flex-col h-full w-full">
-          <div className="flex-1 overflow-auto" ref={scrollRef}>
+          <div className="flex-1 overflow-auto px-2 pb-4" ref={scrollRef}>
             <ConversationView />
           </div>
-          <div className="pt-4 border-t">
+          <div className="pt-4 px-2 border-t">
             <ChatInput />
           </div>
         </div>
