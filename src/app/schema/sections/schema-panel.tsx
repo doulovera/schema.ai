8ed46679e9e741f31214bb901073414e7c56892f
Christopher <chris.glood3011@gmail.com>
2025-05-19 08:46:04 -0400

    feat: syntax highlight

--- src/app/schema/sections/schema-panel.tsx
+++ src/app/schema/sections/schema-panel.tsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ChevronDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import TabTextSelector from "@/components/chat/tab-text-selector";
 import { useChatStore } from "@/stores/chat";
+import Prism from "prismjs";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-shell";
+import "prismjs/themes/prism-okaidia.css";
 
 export default function SchemaPanel({ hidePanel }: { hidePanel: () => void }) {
-  const { chatSchemas } = useChatStore();
+  const { isLoading, chatSchemas } = useChatStore();
   const [activeTab, setActiveTab] = useState(0);
   const scripts = [
     { type: "SQL", text: chatSchemas.sql || "" },
     { type: "MongoDB", text: chatSchemas.mongo || "" },
   ];
+
+  console.log(scripts);
+
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [activeTab, scripts, isLoading]);
+
   return (
     <div className="flex flex-col h-full border-t border-border bg-card text-foreground">
       <div className="p-4 border-b border-border flex justify-between items-center">
@@ -38,11 +49,15 @@ export default function SchemaPanel({ hidePanel }: { hidePanel: () => void }) {
             )
           )}
         </div>
-        <textarea
-          className="w-full h-40 p-2 border border-border rounded-b-md rounded-t-none bg-muted text-foreground resize-none -mt-1"
-          value={scripts[activeTab]?.text}
-          readOnly
-        />
+        <div className="w-full h-40 p-2 border border-border rounded-b-md rounded-t-none bg-muted text-foreground resize-none -mt-1 overflow-auto">
+          <pre className="whitespace-pre-wrap h-full">
+            <code
+              className={`language-${scripts[activeTab]?.type.toLowerCase()}`}
+            >
+              {isLoading ? "Generando script" : scripts[activeTab]?.text}
+            </code>
+          </pre>
+        </div>
       </div>
     </div>
   );
